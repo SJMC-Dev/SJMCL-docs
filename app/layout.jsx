265be60ba5eee5833682950f3f63f0c6f2436fa3
UNIkeEN <94227543+UNIkeEN@users.noreply.github.com>
2025-05-27 20:14:22 +0800

    feat: initial configure nextra framework

---
+++ app/layout.jsx
@@ -0,0 +1,48 @@
+import { Footer, Layout, Navbar } from 'nextra-theme-docs'
+import { Banner, Head } from 'nextra/components'
+import { getPageMap } from 'nextra/page-map'
+import 'nextra-theme-docs/style.css'
+ 
+export const metadata = {
+  // Define your metadata here
+  // For more information on metadata API, see: https://nextjs.org/docs/app/building-your-application/optimizing/metadata
+}
+ 
+// const banner = <Banner storageKey="some-key">Nextra 4.0 is released 🎉</Banner>
+const navbar = (
+  <Navbar
+    logo={<b>SJMCL Docs</b>}
+    // ... Your additional navbar options
+  />
+)
+const footer = <Footer>{new Date().getFullYear()} © SJMCL Team.</Footer>
+ 
+export default async function RootLayout({ children }) {
+  return (
+    <html
+      // Not required, but good for SEO
+      lang="en"
+      // Required to be set
+      dir="ltr"
+      // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
+      suppressHydrationWarning
+    >
+      <Head
+      // ... Your additional head options
+      >
+        {/* Your additional tags should be passed as `children` of `<Head>` element */}
+      </Head>
+      <body>
+        <Layout
+          // banner={banner}
+          navbar={navbar}
+          pageMap={await getPageMap()}
+          docsRepositoryBase="https://github.com/UNIkeEN/SJMCL/tree/main"
+          footer={footer}
+        >
+          {children}
+        </Layout>
+      </body>
+    </html>
+  )
+}
