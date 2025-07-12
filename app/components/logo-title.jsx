b45ba164cff6682fb5ccd0f58bbd0fe8ee7ac8db
Yujie Sun <sunyujie@sjtu.edu.cn>
2025-07-12 16:40:57 +0800

    fix: fix automatically detect locale in logo navigation

--- app/components/logo-title.jsx
+++ app/components/logo-title.jsx
@@ -1,3 +1,5 @@
+'use client'
+
 import Image from "next/image";
 import styles from "../styles/logo-title.module.css";
 import logo from "../../public/images/icons/Logo_128x128.png";
@@ -21,12 +23,31 @@ export const TitleFull = (props) => {
   );
 };
 
+
 export const TitleFullWithLogo = (props) => {
+  const SUPPORTED_LANGS = ['zh', 'en']
+
+  const handleClick = () => {
+    const path = window.location.pathname
+    const parts = path.split('/').filter(Boolean)
+
+    console.log(parts)
+
+    const root = parts[0] || ''
+    const lang = parts[1] && SUPPORTED_LANGS.includes(parts[1]) ? parts[1] :'zh'
+
+    const target = `/${root}/${lang}`
+    window.location.href = target
+  }
+  
   return (
-    <div style={{ display: "flex", alignItems: "center" }}>
+    <div
+      onClick={handleClick}
+      style={{ display: 'flex', alignItems: 'center', cursor: 'pointer' }}
+    >
       <Image src={logo} alt="Logo" width={36} height={36} />
       &nbsp;&nbsp;
       <TitleFull {...props} />
     </div>
-  );
-};
+  )
+}
