01f5c620675593d031f1b1641fd67e09b35a8fc2
Yujie Sun <sunyujie@sjtu.edu.cn>
2025-07-12 18:22:41 +0800

    refactor: adjust layout code structure

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,27 +1,10 @@
 import { ColorModeSyncWrapper } from './components/color-mode-wrapper'
-import { Head } from 'nextra/components'
 import './styles/global.css'
 
 export default function RootLayout({ children }) {
   return (
-    <html
-      // Not required, but good for SEO
-      lang="en"
-      // Required to be set
-      dir="ltr"
-      // Suggested by `next-themes` package https://github.com/pacocoursey/next-themes#with-app
-      suppressHydrationWarning
-    >
-      <Head
-      // ... Your additional head options
-      >
-        {/* Your additional tags should be passed as `children` of `<Head>` element */}
-      </Head>
-      <body>
-        <ColorModeSyncWrapper>
-          {children}
-        </ColorModeSyncWrapper>
-      </body>
-    </html>
+    <ColorModeSyncWrapper>
+      {children}
+    </ColorModeSyncWrapper>
   )
 }
